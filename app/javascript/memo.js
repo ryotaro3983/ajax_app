b173299bfa53eac2c019f36298f9f0853ce0060d
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-09-13 14:34:43 +0900

    JSでレスポンス処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,21 @@
+//onload内で定義していた変数html = の部分を buildHTML として切り出し
+const buildHTML = (XHR) => {
+  //postsコントローラーのcreateアクションにrender json: {post: post}と記述されているため取り出せる
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+    //投稿後に新たに生成されたHTMLを返り値としている
+  return html;
+};
+
+
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {  //e はイベントオブジェクトといい、イベント発生時の情報を持っている。
@@ -9,6 +27,23 @@ function post (){
     XHR.open("POST", "/posts", true);     //リクエストの内容を指定
     XHR.responseType = "json";      //JSON形式でレスポンスを返すよう指定
     XHR.send(formData);       //フォームに入力されたデータを送信(send)
+    //onloadプロパティでリクエスト送信が成功したときの処理を記述
+    XHR.onload = () => {
+      //XHR.statusにはHTTPステータスコードが格納されている
+      if (XHR.status != 200) {
+        //XHR.statusTextのはコードに応じたメッセージが格納される
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        //return null; でjavaの処理から抜け出す
+        //エラーが出た場合この下の処理を実行させないため
+        return null;
+      };
+      //HTMLを生成する記述
+      const list = document.getElementById("list");
+      //フォームの入力データを取得(投稿後フォームにデータが残っているのを削除する準備)
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
